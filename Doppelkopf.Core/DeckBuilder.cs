377154aa07109aecfbaeb2fd8fa9bb14a25e9fd7
Matthias Schröder <mail@matthias-schroeder.eu>
2016-10-03 15:12:44 +0200

    Added function to create a Doppelkopf deck

---
+++ Doppelkopf.Core/DeckBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doppelkopf.Core {
+    public static class DeckBuilder {
+        public static CardStack GetDoppelkopfDeck() {
+            CardStack deck = new CardStack();
+            for (int i = 1; i <= 2; i++) {
+                foreach (var suit in Enum.GetValues(typeof(Suit))) {
+                    foreach (var rank in Enum.GetValues(typeof(Rank))) {
+                        if ((Rank)rank >= Rank.Ten) {
+                            Card card = new Card((Rank)rank, (Suit)suit);
+                            card.MoveTo(deck);
+                        }
+                    }
+                }
+            }
+            return deck;
+        }
+    }
+}
---
+++ Doppelkopf.Test/DeckBuilderTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Doppelkopf.Core;
+using System.Collections.Generic;
+
+namespace Doppelkopf.Test {
+    [TestClass]
+    public class DeckBuilderTest {
+        [TestMethod]
+        public void DoppelkopfDeckTest() {
+            CardStack deck = DeckBuilder.GetDoppelkopfDeck();
+            Assert.AreEqual(40, deck.Cards.Count);
+
+            //Should have two of each Card with a Rank of 10 or higher
+            foreach (Suit suit in Enum.GetValues(typeof(Suit))) {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank))) {
+                    if(rank >= Rank.Ten) {
+                        int count = 0;
+                        foreach(Card card in deck.Cards) {
+                            if(card.Suit == suit && card.Rank == rank) {
+                                count++;
+                            }
+                        }
+                        Assert.AreEqual(2, count);
+                    }
+                }
+            }
+        }
+    }
+}
