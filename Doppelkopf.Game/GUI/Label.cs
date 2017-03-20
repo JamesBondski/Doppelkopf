2dd99f49e27222f2ad869af15ab3d7cf4817e894
Matthias Schröder <github@matthias-schroeder.eu>
2017-03-20 22:52:47 +0100

    Player names are displayed.

--- Doppelkopf.Game/GUI/Label.cs
+++ Doppelkopf.Game/GUI/Label.cs
@@ -9,23 +9,77 @@ using Microsoft.Xna.Framework;
 
 namespace Doppelkopf.Client.GUI
 {
+    public enum Alignment
+    {
+        Min,
+        Mid,
+        Max
+    }
+
     public class Label : ScreenComponent
     {
         public string Text {
             get; set;
-        }
+        } = "";
 
         public string FontName {
             get; set;
         } = "Arial18";
+
+        public Alignment HorizontalAlignment {
+            get; set;
+        } = Alignment.Min;
+
+        public Alignment VerticalAlignment {
+            get; set;
+        } = Alignment.Min;
 
         public Label(ScreenComponent parent) : base(parent) {
         }
 
         public override void Draw(SpriteBatch batch) {
             base.Draw(batch);
+            Vector2 pos = CalculatePosition();
 
-            batch.DrawString(DoppelkopfGame.Instance.TextRenderer.Fonts[this.FontName], this.Text, new Vector2(this.ScreenArea.X, this.ScreenArea.Y), Color.White);
+            batch.DrawString(DoppelkopfGame.Instance.TextRenderer.Fonts[this.FontName], this.Text, pos, Color.White);
+        }
+
+        private Vector2 CalculatePosition() {
+            Vector2 pos = new Vector2(this.ScreenArea.X, this.ScreenArea.Y);
+            Vector2 size = DoppelkopfGame.Instance.TextRenderer.Fonts[this.FontName].MeasureString(this.Text);
+            pos = AdjustHorizontalPosition(pos, size);
+            pos = AdjustVerticalPosition(pos, size);
+            return pos;
+        }
+
+        private Vector2 AdjustVerticalPosition(Vector2 pos, Vector2 size) {
+            switch (this.VerticalAlignment) {
+                case Alignment.Max:
+                    pos.Y = ScreenArea.Bottom - size.Y;
+                    break;
+                case Alignment.Mid:
+                    pos.Y = pos.Y + this.ScreenArea.Width / 2 - size.Y / 2;
+                    break;
+                case Alignment.Min:
+                    break;
+            }
+
+            return pos;
+        }
+
+        private Vector2 AdjustHorizontalPosition(Vector2 pos, Vector2 size) {
+            switch (this.HorizontalAlignment) {
+                case Alignment.Max:
+                    pos.X = ScreenArea.Right - size.X;
+                    break;
+                case Alignment.Mid:
+                    pos.X = pos.X + this.ScreenArea.Width / 2 - size.X / 2;
+                    break;
+                case Alignment.Min:
+                    break;
+            }
+
+            return pos;
         }
     }
 }
--- Doppelkopf.Game/GUI/MainScreen.cs
+++ Doppelkopf.Game/GUI/MainScreen.cs
@@ -19,11 +19,36 @@ namespace Doppelkopf.Client.GUI
 
         public MainScreen(DoppelkopfGame game) : base(game.Input) {
             this.Area = new Rectangle(0, 0, game.GraphicsDevice.Viewport.Width, game.GraphicsDevice.Viewport.Height);
-            new HandDisplay(this, game.Runner.Player, new Rectangle(0, game.GraphicsDevice.Viewport.Height - CardRenderer.DefaultRenderSize.Y - 20, game.GraphicsDevice.Viewport.Width, CardRenderer.DefaultRenderSize.Y));
+            new HandDisplay(this, game.Runner.Player, new Rectangle(0, game.GraphicsDevice.Viewport.Height - CardRenderer.DefaultRenderSize.Y - 40, game.GraphicsDevice.Viewport.Width, CardRenderer.DefaultRenderSize.Y));
             this.trickDisplay = new TrickDisplay(this);
-        }
 
-        
+            new Label(this) {
+                Text = game.Runner.Game.Players[0].Name,
+                Area = new Rectangle(0, game.GraphicsDevice.Viewport.Height - 30, game.GraphicsDevice.Viewport.Width, 20),
+                HorizontalAlignment = Alignment.Mid,
+                ID = "Player0Name"
+            };
+
+            new Label(this) {
+                Text = game.Runner.Game.Players[1].Name,
+                Area = new Rectangle(10, game.GraphicsDevice.Viewport.Height / 2 - CardRenderer.DefaultRenderSize.Y / 2 - 50, 150, 30),
+                ID = "Player1Name"
+            };
+
+            new Label(this) {
+                Text = game.Runner.Game.Players[2].Name,
+                Area = new Rectangle(0, 10, game.GraphicsDevice.Viewport.Width, 30),
+                HorizontalAlignment = Alignment.Mid,
+                ID = "Player2Name"
+            };
+
+            new Label(this) {
+                Text = game.Runner.Game.Players[3].Name,
+                Area = new Rectangle(game.GraphicsDevice.Viewport.Width - 170, game.GraphicsDevice.Viewport.Height / 2 - CardRenderer.DefaultRenderSize.Y / 2 - 50, 150, 30),
+                HorizontalAlignment = Alignment.Max,
+                ID = "Player3Name"
+            };
+        }
 
         public override void Update(GameTime time) {
             base.Update(time);
